85448842ae898e4cf456461fc367120abfc278ed
luizclaudio <luizclaudio@gmail.com>
2012-11-18 16:58:41 -0200

    desenvolvimento

--- controller.py
+++ controller.py
@@ -9,17 +9,32 @@ from google.appengine.ext.webapp import util
 #from model.Categoria import Categoria
 #from model.Coisa import Coisa
 
-def _template_path(filename):
-    return os.path.join(os.path.dirname(__file__), 'view', filename)
+#def _template_path(filename):
+#    return os.path.join(os.path.dirname(__file__), 'view', filename)
+
+def _render_template(filename, values):
+    return template.render(os.path.join(os.path.dirname(__file__), 'view', filename), values)
 
 class LandingPage(webapp.RequestHandler):
     def get(self):
-        template_values = {}        
-        self.response.out.write(template.render(_template_path('landing.html'), template_values))        
+        template_values = {}
+        self.response.out.write(_render_template('landing.html', template_values))
+    def post(self):
+        template_values = {}
+        if self.request.get('username') != '':
+            self.response.out.write(_render_template('home.html', template_values))
+        else:
+            self.response.out.write(_render_template('signup.html', template_values))
+
+class PwdRecoverPage(webapp.RequestHandler):
+    def get(self):
+        template_values = {}
+        self.response.out.write(_render_template('pwdrecover.html', template_values))
 
 def main():
-    application = webapp.WSGIApplication([('/', LandingPage), 
-                                            ('/landing.*', LandingPage),                                           
+    application = webapp.WSGIApplication([('/', LandingPage),
+                                            ('/landing.*', LandingPage),
+                                            ('/pwdrecover.*', PwdRecoverPage),
                                         ],
                                          debug=True)
     util.run_wsgi_app(application)
