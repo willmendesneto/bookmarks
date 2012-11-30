99803d408fd4445ec5e8eafc169a2242f8adeaf3
Luiz Claudio Silva <luizclaudio@gmail.com>
2012-11-30 18:09:07 -0300

    desenvolvimento
    Signed-off-by: Luiz Claudio Silva <luizclaudio@gmail.com>

--- controller.py
+++ controller.py
@@ -9,14 +9,34 @@ import gmemsess
 
 from model import *
 
-def _render_template(filename, values):
+def _renderTemplate(filename, values):
     return template.render(os.path.join(os.path.dirname(__file__), 'view', filename), values)
+
+def _getUserByName(username):
+    usr = None
+    query = User.all()
+    query.filter('name =', username)
+    if query.count() > 0:
+        usr = query.get()
+    return usr
+
+def _getBookmarksByUsername(username):
+    usr = None
+    query = User.all()
+    query.filter('name =', username)
+    if query.count() > 0:
+        usr = query.get()
+    return usr
+
+def _getBookmarksByUserRef(userkey):
+    bmks = []
+    
 
 class LandingPage(webapp.RequestHandler):
 
     def get(self):
         template_values = {'errors' : [], 'fields' : {} }
-        self.response.out.write(_render_template('landing.html', template_values))
+        self.response.out.write(_renderTemplate('landing.html', template_values))
 
     def post(self):
         errors = []
@@ -29,25 +49,25 @@ class LandingPage(webapp.RequestHandler):
             if self.request.get('password').strip() == '':
                 errors.append('blank_password')
             if len(errors) == 0:
-                query = User.all()
-                query.filter('name =', self.request.get('username').strip())
-                if query.count() > 0:
-                    obj = query.get()
-                    if obj.status != 'active':
+                usr = _getUserByName(self.request.get('username').strip())
+                if usr:
+                    if usr.status != 'active':
                         errors.append('non_active_user')
                     else:    
-                        if obj.password != self.request.get('password').strip():
+                        if usr.password != self.request.get('password').strip():
                             errors.append('wrong_password')
                 else:
                     errors.append('unknown_username')
             if len(errors) == 0:
                 sess = gmemsess.Session(self)
-                sess['username'] = self.request.get('username').strip()
+                #sess['username'] = self.request.get('username').strip()
+                sess['username'] = usr.name
+                sess['userkey'] = usr.key()
                 sess.save()
                 self.redirect('/home')
             else:
                 template_values = {'errors' : errors, 'fields' : fields}
-                self.response.out.write(_render_template('landing.html', template_values))
+                self.response.out.write(_renderTemplate('landing.html', template_values))
         else:
             if self.request.get('newusername').strip() == '':
                 errors.append('blank_newusername')
@@ -80,13 +100,13 @@ class LandingPage(webapp.RequestHandler):
                 self.redirect('/signupconf')
             else:
                 template_values = {'errors' : errors, 'fields' : fields}
-                self.response.out.write(_render_template('landing.html', template_values))
+                self.response.out.write(_renderTemplate('landing.html', template_values))
 
 class SignUpConfPage(webapp.RequestHandler):
 
     def get(self):
         template_values = {}
-        self.response.out.write(_render_template('signupconf.html', template_values))
+        self.response.out.write(_renderTemplate('signupconf.html', template_values))
 
 class HomePage(webapp.RequestHandler):
 
@@ -94,27 +114,55 @@ class HomePage(webapp.RequestHandler):
         sess = gmemsess.Session(self)
         if sess.is_new():
             template_values = {'errors' : ['invalid_session'], 'fields' : {} }
-            self.response.out.write(_render_template('landing.html', template_values))
-        else:
-            bookmarks = []
-            tags = []
+            self.response.out.write(_renderTemplate('landing.html', template_values))
+        else:            
+            #usr = _getUserByName(sess['username'])
+            bookmarks = [ b for b in Bookmark.all().filter('user =', sess['userkey']).run() ] # _getBookmarksByUserRef(sess['userkey']) #[]
+            tags = [sess['userkey']] # _getTagsByUserRef(sess['userkey'])
             template_values = {'username' : sess['username'], 'bookmarks' : bookmarks, 'tags' : tags}
-            self.response.out.write(_render_template('home.html', template_values))
+            self.response.out.write(_renderTemplate('home.html', template_values))
 
     def post(self):
         sess = gmemsess.Session(self)
         if sess.is_new():
             template_values = {'errors' : ['invalid_session'], 'fields' : {} }
-            self.response.out.write(_render_template('landing.html', template_values))
+            self.response.out.write(_renderTemplate('landing.html', template_values))
         else:        
             errors = []
             fields = {
-                'bmark_title' : self.request.get('bmark_title').strip(),
-                'bmark_link' : self.request.get('bmark_link').strip(),
-                'bmark_tags' : self.request.get('bmark_tags').strip()
+                'bmarktitle' : self.request.get('bmarktitle').strip(),
+                'bmarklink' : self.request.get('bmarklink').strip(),
+                'bmarktags' : self.request.get('bmarktags').strip()
             }
+            if self.request.get('bmarktitle').strip() == '':
+                errors.append('blank_bmarktitle')
+            if self.request.get('bmarklink').strip() == '':
+                errors.append('blank_bmarklink')
+            if len(errors) == 0:
+                usr = _getUserByName(sess['username'])                
+                if usr:
+                    bmk = Bookmark(title = self.request.get('bmarktitle').strip(),
+                        link = self.request.get('bmarklink').strip(),
+                        user = usr                    
+                    )
+                    bmk.put()
+                else:
+                    errors.append('user_not_found')
+            if len(errors) == 0:
+                if self.request.get('bmarktags').strip() != '':
+                    nams = self.request.get('bmarktags').strip().split(',')
+                    for nam in nams:
+                        objtag = Tag(name = nam,
+                            user = usr                    
+                        )
+                        objtag.put()
+                        bmktag = BookmarkTag(
+                            bookmark = bmk,
+                            tag = objtag
+                        )
+                        bmktag.put()
             template_values = {'errors' : errors, 'fields' : fields}
-            self.response.out.write(_render_template('home.html', template_values))
+            self.response.out.write(_renderTemplate('home.html', template_values))
         
 class LogOutCmd(webapp.RequestHandler):
 
@@ -127,7 +175,7 @@ class PwdRecoverPage(webapp.RequestHandler):
 
     def get(self):
         template_values = {}
-        self.response.out.write(_render_template('pwdrecover.html', template_values))
+        self.response.out.write(_renderTemplate('pwdrecover.html', template_values))
 
 def main():
     application = webapp.WSGIApplication([('/', LandingPage),
